90d1a2c9629070361215e12e0a9bfe0a6cd25dc3
PawelRadawiec <pawel.radaw@gmail.com>
2018-05-03 11:50:55 +0200

    first commit

---
+++ src/main/java/com/info/ZgloszeniazgApplication.java
@@ -0,0 +1,12 @@
+package com.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZgloszeniazgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZgloszeniazgApplication.class, args);
+    }
+}
