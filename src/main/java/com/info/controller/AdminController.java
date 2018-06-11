adbe432c6e8e8e2f915a2948ed2c9d058ba27c44
pawelr <pawelr@lsnova.pl>
2018-06-11 20:36:26 +0200

    change url, add html

--- src/main/java/com/info/controller/AdminController.java
+++ src/main/java/com/info/controller/AdminController.java
@@ -1,12 +1,18 @@
 package com.info.controller;
 
 import com.info.service.AdminServiceImpl;
+import com.info.service.XlsxReport;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 @RequestMapping(value = "/admin")
@@ -14,6 +20,9 @@ public class AdminController {
 
     @Autowired
     private AdminServiceImpl adminService;
+
+    @Autowired
+    private XlsxReport xlsxReport;
 
     @GetMapping()
     public ModelAndView getAllTeamLeaders(){
@@ -41,5 +50,23 @@ public class AdminController {
         modelAndView.addObject("memberlist", adminService.getTeamMembersByLeader(teamLeaderMail));
         modelAndView.setViewName("details");
         return modelAndView;
+    }
+
+    @RequestMapping(value = "/getFile", method =  RequestMethod.GET)
+    public void allTeamMemberReport(HttpServletResponse response) throws Exception {
+        XSSFWorkbook wb = null;
+        try {
+            wb = this.xlsxReport.generateXlsx();
+
+            response.setContentType("application/vnd.ms-excel");
+            response.setHeader("Content-disposition", "attachment; filename=czlonkowie_druzyn.xlsx");
+            wb.write(response.getOutputStream());
+        } catch (IOException ioe) {
+            throw new RuntimeException("Error writing spreadsheet to output stream");
+        } finally {
+            if (wb != null) {
+                wb.close();
+            }
+        }
     }
 }
