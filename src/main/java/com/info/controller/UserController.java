f9a8d6dc7870c1b3ddaaf4e8bc74008b6214080e
PawelRadawiec <pawel.radaw@gmail.com>
2018-08-27 19:43:56 +0200

    new model with service

---
+++ src/main/java/com/info/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.info.controller;
+
+import com.info.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController()
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+}
---
+++ src/main/java/com/info/service/UserService.java
@@ -0,0 +1,66 @@
+package com.info.service;
+
+import com.info.model.User;
+import com.info.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user  = userRepository.findByUsername(username);
+        if(user == null){
+            throw new UsernameNotFoundException("Invalid username or password");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), getAuthority(user));
+    }
+
+    private Set getAuthority(User user){
+        Set authorities = new HashSet<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority("ROLE_" + role.getName()));
+        });
+        return authorities;
+    }
+
+
+    public List<User> findAll() {
+        List<User> list = new ArrayList<>();
+        userRepository.findAll().iterator().forEachRemaining(list::add);
+        return list;
+    }
+
+
+    public void delete(long id){
+        userRepository.deleteById(id);
+    }
+
+
+    public User findOne(String username){
+        userRepository.findByUsername(username);
+    }
+
+    public User findById(Long id){
+        return userRepository.findById(id).get();
+    }
+
+
+}
